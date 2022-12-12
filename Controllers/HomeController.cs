75b151600c2090b7803663937dcdb6181f4543ce
Chris Dykes <chris.t.c.dykes@gmail.com>
2022-12-12 17:49:16 -0500

    Initialize Dev Blog

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevBlog.Controllers;
+
+public class HomeController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    } 
+}
