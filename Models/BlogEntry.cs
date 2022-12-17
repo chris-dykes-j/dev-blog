11d5818d60891102c2adc49d03fc9ce38bc984e5
Chris Dykes <chris.t.c.dykes@gmail.com>
2022-12-17 10:41:52 -0500

    Make larger test entry

--- Models/BlogEntry.cs
+++ Models/BlogEntry.cs
@@ -3,8 +3,8 @@ namespace DevBlog.Models;
 public class BlogEntry
 {
     public int Id { get; set; }
-    public string Title { get; set; }
+    public string Title { get; set; } = null!;
     public DateTime Date { get; set; }
-    public string Description { get; set; }
-    public string Content { get; set; }
+    public string Description { get; set; } = null!;
+    public string Content { get; set; } = null!;
 }
--- Repository/MockBlogRepository.cs
+++ Repository/MockBlogRepository.cs
@@ -19,14 +19,40 @@ public class MockBlogRepository : IBlogRepository
                 Id = 1,
                 Title = "JavaScript for Dummies",
                 Date = new DateTime(2022, 10, 28),
-                Description = "This won't help",
+                Description = "Why on earth are there two different types of null in JavaScript?!", 
                 Content = "Why on earth are there two different types of null in JavaScript?!" 
+            },
+            new BlogEntry
+            {
+                Id = 2,
+                Title = "COBOL is a sin against humanity",
+                Date = new DateTime(2022, 12, 17),
+                Description = "COBOL is terrible and other hot takes.",
+                Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque fermentum purus " +
+                          "at odio venenatis dapibus. Sed congue odio at neque tempor, nec placerat ante suscipit. " +
+                          "Etiam laoreet tortor ut magna tempus interdum. Pellentesque quis imperdiet quam, non imperdiet neque. " +
+                          "Aenean maximus tellus sed posuere pretium. Aenean semper nec nisl eu ullamcorper.\n\n" +
+                          "Integer accumsan, ante ut faucibus porttitor, tortor lacus vestibulum massa, ut vehicula tortor dolor " +
+                          "ultricies erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus." +
+                          " Mauris commodo, sem quis accumsan cursus, est libero bibendum eros, id ultricies diam sem eget sem. " +
+                          "Ut nec ornare mauris, a blandit libero. Duis non euismod urna. Curabitur pellentesque libero " +
+                          "at elit convallis ornare.\n\n" +
+                          "<img src='images/logo.png' class='entry-image'/> \n\n" +
+                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque fermentum purus " +
+                          "at odio venenatis dapibus. Sed congue odio at neque tempor, nec placerat ante suscipit. " +
+                          "Etiam laoreet tortor ut magna tempus interdum. Pellentesque quis imperdiet quam, non imperdiet neque. " +
+                          "Aenean maximus tellus sed posuere pretium. Aenean semper nec nisl eu ullamcorper.\n\n" +
+                          "Integer accumsan, ante ut faucibus porttitor, tortor lacus vestibulum massa, ut vehicula tortor dolor " +
+                          "ultricies erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus." +
+                          " Mauris commodo, sem quis accumsan cursus, est libero bibendum eros, id ultricies diam sem eget sem. " +
+                          "Ut nec ornare mauris, a blandit libero. Duis non euismod urna. Curabitur pellentesque libero " +
+                          "at elit convallis ornare." 
             }
         };
 
     public IEnumerable<BlogEntry> GetAllEntries()
     {
-        return _entries;
+        return _entries.OrderByDescending(x => x.Date);
     }
 
     public BlogEntry GetBlogEntryById(int id)
